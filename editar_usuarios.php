b64200bce73809a704aa45429580ddacfcdbe38a
FontalvoJ <fontalvomejiajosedavid54@gmail.com>
2022-11-02 18:55:40 -0500

    Add button edit

--- editar_usuarios.php
+++ editar_usuarios.php
@@ -6,11 +6,12 @@
         $apellido = $_POST ['apellido'];
         $cedula = $_POST ['cedula'];
         $correo = $_POST ['correo'];
+        $pass = $_POST['contrasena'];
 
 
-     $query = 'UPDATE tbl_users SET nombre_user=?, apellido_user=?,cedula_user=?,email_user=? WHERE id_users=?';
+     $query = 'UPDATE tbl_users SET nombre_user=?, apellido_user=?,cedula_user=?,email_user=?, pass_user=? WHERE id_users=?';
      $query_prepare = $conn->prepare($query);
-     $query_prepare -> execute(array($nombre,$apellido,$cedula,$correo,$id)); 
+     $query_prepare -> execute(array($nombre,$apellido,$cedula,$correo,$pass,$id)); 
      header('Location:listar_usuarios.php');
     }else {
         echo "<script>alert('Error actualizando los datos');</script>";
